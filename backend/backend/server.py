50ec30e7b613d44b10a694f475fdcb4e0707b915
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-10-28 02:23:41 -0300

    Add a test endpoint at server.

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -1,13 +1,17 @@
 '''
     Module to serve the data from de database
 '''
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 def serve():
     app.run(host='0.0.0.0')
