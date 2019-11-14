738b9ab1c209e87fe3e19a3b6e322610b4f829c6
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-11-14 06:10:37 -0300

    Adapt to python CS.

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -3,94 +3,97 @@
 '''
 from flask import Flask, render_template
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+@APP.route("/")
 def home():
+    '''Server testing endpoint.'''
     return "Hello, World!"
 
-# @app.route("/test")
-# def test():
-#     return render_template("test.html")
-
-# @app.route("/test-button")
-# def testButton():
-#     return render_template("test-button.html")
-
-# @app.route("/1-to-5")
-# def oneToFive():
-#     return render_template("1-to-5.html")
-
-# @app.route("/6-to-9")
-# def sixToNine():
-#     return render_template("6-to-9.html")
-
-@app.route("/1-7")
-def oneToSeven():
+@APP.route("/1-7")
+def one_to_seven():
+    '''Exposes 1 to 7 buttons.'''
     return render_template("1-7.html")
 
-@app.route("/8-14")
-def eightToFourteen():
+@APP.route("/8-14")
+def eight_to_fourteen():
+    '''Exposes 8 to 14 buttons.'''
     return render_template("8-14.html")
 
-@app.route("/1")
-def c4Button():
-    return render_template("button-note.html", note="c4", note_number="1" )
+@APP.route("/1")
+def c4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="c4", note_number="1")
 
-@app.route("/2")
-def d4Button():
-    return render_template("button-note.html", note="d4", note_number="2" )
+@APP.route("/2")
+def d4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="d4", note_number="2")
 
-@app.route("/3")
-def e4Button():
-    return render_template("button-note.html", note="e4", note_number="3" )
+@APP.route("/3")
+def e4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="e4", note_number="3")
 
-@app.route("/4")
-def f4Button():
-    return render_template("button-note.html", note="f4", note_number="4" )
+@APP.route("/4")
+def f4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="f4", note_number="4")
 
-@app.route("/5")
-def g4Button():
-    return render_template("button-note.html", note="g4", note_number="5" )
+@APP.route("/5")
+def g4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="g4", note_number="5")
 
-@app.route("/6")
-def a4Button():
-    return render_template("button-note.html", note="a4", note_number="6" )
+@APP.route("/6")
+def a4_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="a4", note_number="6")
 
-@app.route("/7")
-def g3Button():
-    return render_template("button-note.html", note="g3", note_number="7" )
+@APP.route("/7")
+def g3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="g3", note_number="7")
 
-@app.route("/8")
-def a3Button():
-    return render_template("button-note.html", note="a3", note_number="8" )
+@APP.route("/8")
+def a3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="a3", note_number="8")
 
-@app.route("/9")
-def b3Button():
-    return render_template("button-note.html", note="b3", note_number="9" )
+@APP.route("/9")
+def b3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="b3", note_number="9")
 
-@app.route("/10")
-def f3Button():
-    return render_template("button-note.html", note="f3", note_number="10" )
+@APP.route("/10")
+def f3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="f3", note_number="10")
 
-@app.route("/11")
-def e3Button():
-    return render_template("button-note.html", note="e3", note_number="11" )
+@APP.route("/11")
+def e3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="e3", note_number="11")
 
-@app.route("/12")
-def d3Button():
-    return render_template("button-note.html", note="d3", note_number="12" )
+@APP.route("/12")
+def d3_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="d3", note_number="12")
 
-@app.route("/13")
-def a2Button():
-    return render_template("button-note.html", note="a2", note_number="13" )
+@APP.route("/13")
+def a2_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="a2", note_number="13")
 
-@app.route("/14")
-def g2Button():
-    return render_template("button-note.html", note="g2", note_number="14" )
+@APP.route("/14")
+def g2_button():
+    '''Exposes a note button.'''
+    return render_template("button-note.html", note="g2", note_number="14")
 
 def serve():
-    app.run(host='0.0.0.0')
+    '''Exposes server to all interfaces.'''
+    APP.run(host='0.0.0.0')
 
+'''Entry point to serve the endpoints via poetry.'''
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
